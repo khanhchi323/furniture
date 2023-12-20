d635068179823d48c54facb00f58501c09c82974
khanhchi323 <npkc323@gmai.com>
2023-12-20 10:07:50 +0700

    push bg

--- react/src/components/public/header.jsx
+++ react/src/components/public/header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Bg from "./assets/public"
 export default function Header() {
     return (
-      <div style={{backgroundColor: "lightblue", height: "10vh", width:"100%"}}>
-        Header
-      </div>
+      <div className="bg-cover bg-center h-screen" style={{backgroundImage: 'url(${Bg})'}}>
+  
+</div>
     )
   }
   
