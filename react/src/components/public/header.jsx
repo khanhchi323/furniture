3c7a43dfb567813de50a57f3c9435ac482f2800a
khanhchi323 <npkc323@gmai.com>
2023-12-20 13:11:28 +0700

    bg

--- react/src/components/public/header.jsx
+++ react/src/components/public/header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Bg from "../../views/public/Home.jsx";
+import Background from "../../assets/public/bg-main.svg";
+
 export default function Header() {
-    return (
-        <div
-            className="bg-cover bg-center h-screen"
-            style={{ backgroundImage: 'url("../assets/public/bg-shop.jpg)' }}
-        ></div>
-    );
+  return (
+    <div className="bg-cover bg-center h-screen" style={{ backgroundImage: `url(${Background})` }}>
+      <div className="h-[8vh] text-white">
+        Chổ này là thanh nav
+      </div>
+    </div>
+  );
 }
