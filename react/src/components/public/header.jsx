0f52f07a1aa9d9ddc8f694c967c6a0b256be1d84
khanhchi323 <npkc323@gmai.com>
2023-12-20 13:18:43 +0700

    push again

--- react/src/components/public/header.jsx
+++ react/src/components/public/header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Background from "../../assets/public/bg-main.svg";
-
+import Logo from "react/public/logo.svg"
 export default function Header() {
   return (
     <div className="bg-cover bg-center h-screen" style={{ backgroundImage: `url(${Background})` }}>
+      <div className="logo">
+        <img src= {Logo} alt="" />
+      </div>
       <div className="h-[8vh] text-white">
         Chổ này là thanh nav
       </div>
