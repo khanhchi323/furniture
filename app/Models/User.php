de145894969e8e3483d47f7c780a6c6dfe082ddc
khanhchi323 <npkc323@gmai.com>
2023-12-27 16:08:12 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,35 +11,44 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    protected $table = 'user';
+    protected $fillable = ['user_id', 'type_account_id',
+     'username', 'email','email_verified_at','password',
+     'remember_token', 'avt_image', 'first_name', 'last_name', 'telephone', 'created_at', 'modified_at'];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $primaryKey = 'user_id';
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
-        'remember_token',
+        'remember_token'
+        
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    // public function tokens()
+    // {
+    //     return $this->hasMany(PersonalAccessToken::class);
+    // }
+
+
+    // Trong model User.php
+    // protected static function boot()
+    // {
+    //     parent::boot();
+
+    //     static::created(function ($user) {
+    //         $user->updated_at = now();
+    //         $user->save();
+    //     });
+    // }
+
+
+    // public function user_address()
+    // {
+    //     return $this->hasMany(user_address::class, 'user_id', 'user_id');
+    // }
 }
---
+++ database/migrations/2023_12_26_162024_user.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->bigIncrements('user_id');
+            $table->unsignedBigInteger('role_id');
+            $table->string('username', 50);
+            $table->string('password', 150);
+            $table->string('avt_image', 50);
+            $table->string('first_name', 20);
+            $table->string('last_name', 20);
+            $table->string('telephone', 11);
+            $table->timestamps();
+
+            // Khóa ngoại role_id
+            //$table->foreign('role_id')->references('role_id')->on('role')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+};
