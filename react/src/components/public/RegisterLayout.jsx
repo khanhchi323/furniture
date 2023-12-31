af95d39cde62a52b174bcc3fe6bc4194848be6ff
Duongbang194 <nguyenduongbang194@gmail.com>
2023-12-31 09:36:33 +0700

    register-băng

--- react/src/components/public/RegisterLayout.jsx
+++ react/src/components/public/RegisterLayout.jsx
@@ -1,39 +1,39 @@
 import React from "react";
-import Background from "../../assets/public/noithat-intro5.jpeg"
+import Background from "../../assets/public/8.jpg"
 export default function registerForm() {
     return (
 
         <div className="font-sans m-0 p-0 flex items-center justify-center bg-white h-screen" style={{ backgroundImage: `url(${Background})` }} >
-            <div className="text-black bg-transparent w-96 p-20px rounded-lg shadow-md z-10 shadow-2xl">
-                <h2 className="text-lg ml-20">Register</h2>
+            <div className="text-white  w-96 p-20px rounded-lg shadow-lg shadow-neutral-950 z-10 ">
+                <h2 className="text-2xl font-bold mb-4 text-center mt-4">Register</h2>
                 <div className="mr-25px mb-20px rounded-sm flex">
                     <div className="mr-25px mb-20px rounded-sm">
-                        <label className="text-base ml-6 mb-2 block" for="username">First name</label>
-                        <input className="text-base w-20 p-10px ml-6 border-solid outline rounded bg-transparent text-white" type="text" id="username" name="username" required />
+                        <label className="text-base ml-8 my-1.5 block" for="username">First name</label>
+                        <input className="text-base w-36 p-10px ml-8 border-solid outline rounded-xl bg-transparent text-white" type="text" id="username" name="username" required />
                     </div>
                     <div className="mr-25px mb-20px rounded-sm ">
-                        <label className="text-base ml-6 mb-2 block" for="username">Last name</label>
-                        <input className="text-base w-25 p-10px ml-6 border-solid outline rounded bg-transparent text-white" type="text" id="username" name="username" required />
+                        <label className="text-base ml-7 my-1.5 block" for="username">Last name</label>
+                        <input className="text-base w-36 p-10px ml-7 border-solid outline rounded-xl bg-transparent text-white" type="text" id="username" name="username" required />
                     </div>
                 </div>
                 <div className="mr-25px mb-20px rounded-sm">
-                    <label className="text-base ml-6 mb-2 block" for="username">User name</label>
-                    <input className="text-base w-80 p-10px ml-6 border-solid outline rounded bg-transparent text-white" type="text" id="username" name="username" required />
+                    <label className="text-base ml-8 my-1.5 block" for="username">User name</label>
+                    <input className="text-base w-80 p-10px ml-8 border-solid outline rounded-xl bg-transparent text-white" type="text" id="username" name="username" required />
                 </div>
                 <div className="mr-25px mb-20px rounded-sm">
-                    <label className="text-base mb-2 ml-6 block" for="password">Password</label>
-                    <input className="text-base w-80 p-10px ml-6 border-solid outline rounded bg-transparent text-white" type="password" id="password" name="password" required />
+                    <label className="text-base my-1.5 ml-8 block" for="password">Password</label>
+                    <input className="text-base w-80 p-10px ml-8 border-solid outline rounded-xl bg-transparent text-white" type="password" id="password" name="password" required />
                 </div>
                 <div className="mr-25px mb-20px rounded-sm">
-                    <label className="text-base ml-6 mb-2 block" for="numberphone">Number Phone</label>
-                    <input className="text-base w-80 p-10px ml-6 border-solid outline rounded bg-transparent text-white" type="text" id="numberphone" name="numberphone" required />
+                    <label className="text-base ml-8 my-1.5 block" for="numberphone">Number Phone</label>
+                    <input className="text-base w-80 p-10px ml-8 border-solid outline rounded-xl bg-transparent text-white" type="text" id="numberphone" name="numberphone" required />
                 </div>
                 <div className="mr-25px mb-20px rounded-sm">
-                    <label className="text-base mb-2 ml-6 block" for="email">Email</label>
-                    <input className="text-base w-80 p-10px ml-6 border-solid outline rounded bg-transparent text-white" type="email" id="email" name="email" required />
+                    <label className="text-base my-1.5 ml-8 block" for="email">Email</label>
+                    <input className="text-base w-80 p-10px ml-8 border-solid outline rounded-xl bg-transparent text-white" type="email" id="email" name="email" required />
                 </div>
-                <button className="bg-amber-300 text-white p-2 py-1 mt-3 ml-20 border-none text-lg cursor-pointer rounded-3xl hover:bg-amber-200" type="submit">Đăng kí</button>
-                <div className="pt-3 pb-3 ml-20 ">I had an account <a className="decoration-none	" href="/login">Log in</a>
+                <button className="bg-amber-300 text-white px-2 py-1 my-3 border-none text-lg cursor-pointer rounded-full hover:bg-amber-200" type="submit">Register</button>
+                <div className="pt-3 pb-3 text-center shadow.md text-white">I already have an account <a className="decoration-none" href="/login">Log in</a>
 
                 </div>
             </div>
