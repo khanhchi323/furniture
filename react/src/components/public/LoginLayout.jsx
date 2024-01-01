3f5446c20698d4f36f46817da0d1e274bd55c635
Duongbang194 <nguyenduongbang194@gmail.com>
2024-01-01 22:37:30 +0700

    register-băng

--- react/src/components/public/LoginLayout.jsx
+++ react/src/components/public/LoginLayout.jsx
@@ -3,32 +3,32 @@ import Background from "../../assets/public/bglog.jpg";
 export default function LoginLayout() {
   return (
     <div className="font-sans m-0 p-0 flex items-center justify-center h-screen bg-cover bg-no-repeat"
-    style={{ backgroundImage: `url(${Background})` }} >
-         
-    <div className="text-white w-500 p-20 rounded-8 bg-slate-800 bg-opacity-10 ">
+      style={{ backgroundImage: `url(${Background})` }} >
+
+      <div className="text-white w-500 p-20 rounded-8 bg-slate-800 bg-opacity-10 ">
         <h2 className="text-2xl font-bold mb-4 text-center mt-4">Login</h2>
         <div className="mr-25 mb-3 rounded-sm">
-            <label className="text-base mb-2 block" for="username">Username:</label>
-            <input type="text" id="username" className="text-lg w-full p-10px border-solid outline rounded bg-transparent text white"/>
+          <label className="text-base mb-2 block" for="username">Username:</label>
+          <input type="text" id="username" className="text-lg w-full p-10px border-solid outline rounded bg-transparent text white" />
         </div>
 
         <div className="mr-25 mb-3 rounded-sm">
-            <label className="text-base mb-2 block" for="password">Password:</label>
-            <input type="password" id="password" className="text-lg w-full p-10px border-solid outline rounded bg-transparent text white"/>
+          <label className="text-base mb-2 block" for="password">Password:</label>
+          <input type="password" id="password" className="text-lg w-full p-10px border-solid outline rounded bg-transparent text white" />
         </div>
-        
+
         <div>
-          <label className="text-base mb-2 block"><input type="checkbox"/> Remeber me </label>
-      </div>
+          <label className="text-base mb-2 block"><input type="checkbox" /> Remeber me </label>
+        </div>
 
-      <button className="bg-amber-300 text-white p-2 py-1 ml-3 mt-2 border-none text-lg cursor-pointer rounded-3xl hover:bg-amber-200" type="submit">Login</button>
+        <button className="bg-amber-300 text-white p-2 py-1 ml-3 mt-2 border-none text-lg cursor-pointer rounded-3xl hover:bg-amber-200" type="submit">Login</button>
 
-      <div>
-        <p>Don't have an account?<a 
-        href="/register"> Register</a></p>
+        <div>
+          <p>Don't have an account?<a
+            href="/register"> Register</a></p>
+        </div>
+
       </div>
-      
-    </div>
     </div>
   );
 }
--- react/src/components/public/RegisterLayout.jsx
+++ react/src/components/public/RegisterLayout.jsx
@@ -9,30 +9,32 @@ export default function registerForm() {
                 <div className="mr-25px mb-20px rounded-sm flex">
                     <div className="mr-25px mb-20px rounded-sm">
                         <label className="text-base ml-8 my-1.5 block" for="username">First name</label>
-                        <input className="text-base w-36 p-10px ml-8 border-solid outline rounded-xl bg-transparent text-white" type="text" id="username" name="username" required />
+                        <input className="text-base w-36 pl-2 pb-0.4 ml-8 border-solid outline rounded-xl bg-transparent text-white" type="text" id="username" name="username" required />
                     </div>
                     <div className="mr-25px mb-20px rounded-sm ">
                         <label className="text-base ml-7 my-1.5 block" for="username">Last name</label>
-                        <input className="text-base w-36 p-10px ml-7 border-solid outline rounded-xl bg-transparent text-white" type="text" id="username" name="username" required />
+                        <input className="text-base w-36 pl-2 pb-0.4 ml-7 border-solid outline rounded-xl bg-transparent text-white" type="text" id="username" name="username" required />
                     </div>
                 </div>
                 <div className="mr-25px mb-20px rounded-sm">
                     <label className="text-base ml-8 my-1.5 block" for="username">User name</label>
-                    <input className="text-base w-80 p-10px ml-8 border-solid outline rounded-xl bg-transparent text-white" type="text" id="username" name="username" required />
+                    <input className="text-base w-80 pl-2 pb-0.4 ml-8 border-solid outline rounded-xl bg-transparent text-white" type="text" id="username" name="username" required />
                 </div>
                 <div className="mr-25px mb-20px rounded-sm">
                     <label className="text-base my-1.5 ml-8 block" for="password">Password</label>
-                    <input className="text-base w-80 p-10px ml-8 border-solid outline rounded-xl bg-transparent text-white" type="password" id="password" name="password" required />
+                    <input className="text-base w-80 pl-2 pb-0.4 ml-8 border-solid outline rounded-xl bg-transparent text-white" type="password" id="password" name="password" required />
                 </div>
                 <div className="mr-25px mb-20px rounded-sm">
                     <label className="text-base ml-8 my-1.5 block" for="numberphone">Number Phone</label>
-                    <input className="text-base w-80 p-10px ml-8 border-solid outline rounded-xl bg-transparent text-white" type="text" id="numberphone" name="numberphone" required />
+                    <input className="text-base w-80 pl-2 pb-0.4 ml-8 border-solid outline rounded-xl bg-transparent text-white" type="text" id="numberphone" name="numberphone" required />
                 </div>
                 <div className="mr-25px mb-20px rounded-sm">
                     <label className="text-base my-1.5 ml-8 block" for="email">Email</label>
-                    <input className="text-base w-80 p-10px ml-8 border-solid outline rounded-xl bg-transparent text-white" type="email" id="email" name="email" required />
+                    <input className="text-base w-80 pl-2 pb-0.4 ml-8 border-solid outline rounded-xl bg-transparent text-white" type="email" id="email" name="email" required />
                 </div>
-                <button className="bg-amber-300 text-white px-2 py-1 my-3 border-none text-lg cursor-pointer rounded-full hover:bg-amber-200" type="submit">Register</button>
+                <div className="text-center">
+                    <button className=" bg-amber-300 text-white  px-3 py-1 mt-3 text-lg cursor-pointer rounded-full hover:bg-amber-200" type="submit">Register</button>
+                </div>
                 <div className="pt-3 pb-3 text-center shadow.md text-white">I already have an account ? <a className="decoration-none" href="/login">Log in</a>
 
                 </div>
