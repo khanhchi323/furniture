ad17e88ac85139c60a9e98a2116d209f5b21c65d
khanhchi323 <npkc323@npkc323gmail.com>
2024-01-07 10:21:22 +0700

    aa

--- react/src/components/public/footer.jsx
+++ react/src/components/public/footer.jsx
@@ -1,8 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 export default function Footer() {
     return (
-      <div style={{backgroundColor: "lightpink", height: "10vh"}}>
-        Footer
+      <div className=" bg-[#EEEDEA]">
+        <div className="flex justify-between items-center p-[10vh]">
+          <div>
+            <h4>Company</h4>
+            <div className="flex flex-col">
+              <Link to="">Home</Link>
+              <Link to="">Categories</Link>
+              <Link to="">About us</Link>
+              <Link to="">Contact us</Link>
+            </div>  
+          </div>
+
+          <div>
+          <h4>Support</h4>
+          <div className="flex flex-col">
+            <Link to="">Help Center</Link>
+            <Link to="">Safety Center</Link>
+            <Link to="">Community Guidelines</Link>
+          </div>          
+          </div>
+
+          <div>
+          <h4>Legal</h4>
+          <div className="flex flex-col">
+            <Link to="">Cookies Policy</Link>
+            <Link to="">Privacy Policy</Link>
+            <Link to="">Terms of Service</Link>
+          </div>
+          </div>
+
+          <div>
+            <p>Trụ sở chính: 18A/1 Cộng Hòa</p>
+          </div>
+
+        </div>
       </div>
     )
   }
